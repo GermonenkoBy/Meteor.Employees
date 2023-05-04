093aa4b23f5b51ab1c587fa9d79d28ceaed46030
Slava Germonenko <slava.germonenko@gmail.com>
2023-05-04 23:27:06 +0200

    Send messages to the ASB once employee is changed (created/updated) or removed.

---
+++ src/Meteor.Employees.Core/Dtos/EmployeeChangedMessage.cs
@@ -0,0 +1,18 @@
+﻿namespace Meteor.Employees.Core.Dtos;
+
+using Models.Enums;
+
+public record struct EmployeeChangedMessage
+{
+    public int Id;
+
+    public string FirstName;
+
+    public string LastName;
+
+    public string EmailAddress;
+
+    public string PhoneNumber;
+
+    public EmployeeStatuses Status;
+}
